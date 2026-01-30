3308423b8e1b773df0d42a032a4d92ab503d4cc9
mithun <mithun-io@outlook.com>
2026-01-30 10:46:33 +0530

    Create StaticMethodExample.java

---
+++ src/com/advance/StaticMethodExample.java
@@ -0,0 +1,33 @@
+package com.advance;
+
+interface PaymentUtil {
+	// static method (utility logic)
+	static boolean validateAmount(double amount) {
+		return amount > 0;
+	}
+}
+
+class UpiPayment {
+
+	public void pay(double amount) {
+
+		if (!PaymentUtil.validateAmount(amount)) {
+			System.out.println("invalid amount");
+			return;
+		}
+
+		System.out.println("upi payment successful");
+		System.out.println("amount: " + amount);
+	}
+}
+
+public class StaticMethodExample {
+
+	public static void main(String[] args) {
+
+		UpiPayment payment = new UpiPayment();
+
+		payment.pay(1000);
+		payment.pay(-500);
+	}
+}

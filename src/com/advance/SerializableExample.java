21dea2ef306ce02d0be0d3a0cd5894a53fa2cc58
mithun <mithun-io@outlook.com>
2026-01-30 07:00:04 +0530

    Update SerializableExample.java

--- src/com/advance/SerializableExample.java
+++ src/com/advance/SerializableExample.java
@@ -1,67 +1,54 @@
 package com.advance;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 // case 1: serialize
 class Student implements Serializable {
-	int id;
-	String name;
+	int empno;
+	String ename;
+	
+	public Student(int empno, String ename) {
+		super();
+		this.empno = empno;
+		this.ename = ename;
+	}
 
-	Student(int id, String name) {
-		this.id = id;
-		this.name = name;
+	@Override
+	public String toString() {
+		return "Student [empno=" + empno + ", ename=" + ename + "]";
 	}
 }
 
-class SerializableExample {
-	public static void main(String[] args) throws Exception {
-
-		Student s = new Student(1, "smith");
-
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("student.txt"));
-
-		oos.writeObject(s);
-		oos.close();
-
+public class SerializableExample {
+	public static void main(String[] args) throws FileNotFoundException, IOException  {
+		Student student = new Student(7902, "smith");
+		
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("employee.txt"));
+		objectOutputStream.writeObject(student);
+		objectOutputStream.close();
+		
 		System.out.println("object serialized successfully");
 	}
 }
 
 // case 2: deserialize
-package com.advance;
-
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
-
-class DeserializationExample {
-	public static void main(String[] args) throws Exception {
-
-		ObjectInputStream ois =	new ObjectInputStream(new FileInputStream("student.txt"));
-
-		Student s = (Student) ois.readObject();
-
-		ois.close();
-
-		System.out.println(s.id);
-		System.out.println(s.name);
+public class DeSerializableExample {
+	public static void main(String[] args) throws ClassNotFoundException, IOException  {
+		ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("employee.txt"));
+		
+		Student student = (Student) objectInputStream.readObject();
+		objectInputStream.close();
+		
+		System.out.println(student.empno);
+		System.out.println(student.ename);
+		
+		System.out.println("object de-serialized successfully");
 	}
 }
 
-// case 3:
-class Employee {
-	int id = 10;
-}
-
-class SerializableFail {
-	public static void main(String[] args) throws Exception {
-
-		Employee e = new Employee();
-
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("emp.txt"));
-
-		oos.writeObject(e); // Exception here
-		// NotSerializableException
-	}
-}

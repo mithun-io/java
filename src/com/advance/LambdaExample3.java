e8bb6e09b1982bd2273e7ce4a3ce670ce58eff98
mithun <mithun-io@outlook.com>
2026-01-30 06:35:54 +0530

    Update LambdaExample3.java

--- src/com/advance/LambdaExample3.java
+++ src/com/advance/LambdaExample3.java
@@ -1,25 +1,19 @@
 package com.advance;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Triangle {
-	double area(double a, double b) {
-		return 0.5 * a * b;
+	double area(double x, double y) {
+		return 0.5 * x * y;
 	}
 }
 
-class Main {
+class Example {
 	public static void main(String[] args) {
 		Triangle triangle = new Triangle();
-
-		// List of base & height values
-		List<double[]> list = new ArrayList<>();
-		list.add(new double[] { 10, 5 });
-		list.add(new double[] { 8, 4 });
-		list.add(new double[] { 6, 3 });
-
-		// forEach loop (direct output)
-		list.forEach(values -> System.out.println(triangle.area(values[0], values[1])));
+	
+		List<double[]> list = Arrays.asList(new double[] {1, 3}, new double[] {8, 9}, new double[] {5, 6}, new double[] {2, 4});
+		list.forEach(x -> System.out.println(triangle.area(x[0], x[1])));
 	}
 }

e7cb163c5fc67f570eea4eeae5fa8fe2f11699ba
mithun <mithun-io@outlook.com>
2026-01-30 19:27:35 +0530

    Update StaticMethodExample.java

--- src/com/advance/StaticMethodExample.java
+++ src/com/advance/StaticMethodExample.java
@@ -8,25 +8,19 @@ interface PaymentUtil {
 }
 
 class UpiPayment {
-
 	public void pay(double amount) {
-
 		if (!PaymentUtil.validateAmount(amount)) {
 			System.out.println("invalid amount");
 			return;
 		}
-
 		System.out.println("upi payment successful");
 		System.out.println("amount: " + amount);
 	}
 }
 
 public class StaticMethodExample {
-
 	public static void main(String[] args) {
-
 		UpiPayment payment = new UpiPayment();
-
 		payment.pay(1000);
 		payment.pay(-500);
 	}
